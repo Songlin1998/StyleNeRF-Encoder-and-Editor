72dc435fb11bc4abaa12db515c03224d8a192733
Songlin Yang <97295756+Songlin1998@users.noreply.github.com>
2024-10-03 10:41:20 +0800

    Add files via upload

---
+++ configs/paths_config.py
@@ -0,0 +1,21 @@
+## Pretrained models paths
+e4e = '/hd4/yangsonglin-3D/STIT_stylenerf_3da/pretrained_models/e4e_ffhq_encode.pt'
+stylegan2_ada_ffhq = '/hd4/yangsonglin-3D/STIT_stylenerf_3da/stylenerf-ffhq-config-basic/ffhq_1024.pkl'
+ir_se50 = './pretrained_models/model_ir_se50.pth'
+
+## Dirs for output files
+checkpoints_dir = './checkpoints'
+
+## Keywords
+pti_results_keyword = 'STIT'
+
+## Edit directions
+interfacegan_folder = 'editings/w_directions/'
+styleclip_fs3 = 'editings/styleclip_global/fs3.npy'
+
+stylespace_mean_std = 'editings/styleclip_global/S_mean_std'
+
+shape_predictor_path = '/hd4/yangsonglin-3D/STIT_stylenerf_3da/pretrained_models/shape_predictor_68_face_landmarks.dat'
+
+segmentation_model_path = '/hd4/yangsonglin-3D/STIT_stylenerf_3da/pretrained_models/79999_iter.pth'
+
